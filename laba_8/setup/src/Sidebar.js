7eb7c00e4a6baa3942da6da02d18477aa9e1d6b7
Maxim Osadchiy <maksimgarsia709@gmail.com>
2025-11-02 12:52:08 +0200

    ЛБ8

---
+++ laba_8/setup/src/Sidebar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { useGlobalContext } from "./context";
+
+const Sidebar = () => {
+  const { isSideBarOpen, closeSideBar } = useGlobalContext();
+
+  return (
+    <aside className={`${isSideBarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo" />
+        <button className="close-btn" onClick={closeSideBar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map(({ id, url, text, icon }) => (
+          <li key={id}>
+            <a href={url}>
+              {icon} {text}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className="social-icons">
+        {social.map(({ id, url, icon }) => (
+          <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
